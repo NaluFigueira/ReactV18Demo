641313226d3698688bc681ac8c99c5927def8a2b
NaluFigueira <anachfigueira@gmail.com>
2022-05-20 16:16:07 -0300

    Configuring tailwindcss and prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
-import React from "react";
+import React from 'react';
 
 function App() {
-  return <h1>Hello World</h1>;
+	return (
+		<div className="bg-slate-900 p-24 h-screen">
+			<h1 className="text-white text-6xl">React v18</h1>
+			<h2 className="text-white text-4xl my-6">Introdução</h2>
+		</div>
+	);
 }
 
 export default App;
