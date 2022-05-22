282da2b84d53b257e25f7f3b0427e1f53ccee36e
NaluFigueira <anachfigueira@gmail.com>
2022-05-22 00:18:12 -0300

    Adding new pages to the list

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,25 @@
 import React from 'react';
 import PageList from './components/PageList';
+import Adoption from './pages/Adoption';
+import AutomaticBatching from './pages/AutomaticBatching';
+import Debounce from './pages/Debounce';
+import FlushAsync from './pages/FlushAsync';
 import Introduction from './pages/Introduction';
 import MainFeature from './pages/MainFeature';
+import ServerComponents from './pages/ServerComponents';
 
 function App() {
-	const pages = [<Introduction />, <MainFeature />];
+	const pages = [
+		<Introduction />,
+		<MainFeature />,
+		<Adoption />,
+		<ServerComponents />,
+		<AutomaticBatching />,
+		<FlushAsync />,
+		<Debounce />,
+	];
 	return (
-		<div className="bg-neutral-900 p-24 h-screen">
+		<div className="bg-neutral-900 p-16 h-screen overflow-y-scroll">
 			<PageList pages={pages} finalPageNumber={pages.length} />
 		</div>
 	);
