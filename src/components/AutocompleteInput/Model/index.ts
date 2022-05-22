ddcf6cf9cb5306a653ca03c834166228d12fc524
NaluFigueira <anachfigueira@gmail.com>
2022-05-22 00:17:35 -0300

    Creating autocomplete input base data

---
+++ src/components/AutocompleteInput/Model/index.ts
@@ -0,0 +1,51 @@
+export interface Item {
+	id: string;
+	name: string;
+}
+
+export const items: Array<Item> = [
+	{
+		id: '1',
+		name: 'Antartica',
+	},
+	{
+		id: '2',
+		name: 'Bohemia',
+	},
+	{
+		id: '3',
+		name: 'Brahma',
+	},
+	{
+		id: '4',
+		name: 'Budweiser',
+	},
+	{
+		id: '5',
+		name: 'Colorado',
+	},
+	{
+		id: '6',
+		name: 'Corona',
+	},
+	{
+		id: '7',
+		name: 'Hoegaarden',
+	},
+	{
+		id: '8',
+		name: 'Patagonia',
+	},
+	{
+		id: '9',
+		name: 'Skol',
+	},
+	{
+		id: '10',
+		name: 'Spaten',
+	},
+	{
+		id: '11',
+		name: 'Stella Artois',
+	},
+];
